cae37609656010a386fd33fb5db2af9ec4c6fa31
Dmsrdnv <dmsrdnv@gmail.com>
2019-12-29 16:12:04 +0300

    Letters tab was added

--- app/src/main/java/pl/holker/music_app_android/di/modules/FragmentsModule.kt
+++ app/src/main/java/pl/holker/music_app_android/di/modules/FragmentsModule.kt
@@ -4,6 +4,8 @@ import dagger.Module
 import dagger.android.ContributesAndroidInjector
 import pl.holker.music_app_android.functionalities.location_notes.LocationNotesFragment
 import pl.holker.music_app_android.functionalities.typical_notes.TypicalNotesFragment
+import pl.holker.music_app_android.functionalities.location_letters.LocationLettersFragment
+import pl.holker.music_app_android.functionalities.letters.LettersFragment
 
 @Module
 abstract class FragmentsModule {
@@ -12,4 +14,10 @@ abstract class FragmentsModule {
 
     @ContributesAndroidInjector
     abstract fun contributeLocationNoteFragment(): LocationNotesFragment
+
+    @ContributesAndroidInjector
+    abstract fun contributeLettersFragment(): LettersFragment
+
+    @ContributesAndroidInjector
+    abstract fun contributeLocationLettersFragment(): LocationLettersFragment
 }
