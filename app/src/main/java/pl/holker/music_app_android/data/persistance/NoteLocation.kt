0860581a1333463b5d04820efad14c3274ae9828
Dmsrdnv <dmsrdnv@gmail.com>
2019-12-29 16:01:30 +0300

    Letters tab was added

--- app/src/main/java/pl/holker/music_app_android/data/persistance/NoteLocation.kt
+++ app/src/main/java/pl/holker/music_app_android/data/persistance/NoteLocation.kt
@@ -4,9 +4,9 @@ import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
-@Entity(tableName = "location_letters")
+@Entity(tableName = "location_notes")
 data class NoteLocation(
-    @PrimaryKey(autoGenerate = true) @ColumnInfo(name = "letter_location_id") val id: Int = 0,
+    @PrimaryKey(autoGenerate = true) @ColumnInfo(name = "note_location_id") val id: Int = 0,
     @ColumnInfo(name = "title") val title: String,
     @ColumnInfo(name = "content") val content: String,
     @ColumnInfo(name = "x") val x: Float,
